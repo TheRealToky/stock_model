caa4ec6b03f07784baf58d71d448925440d62866
TheRealToky <tokydisaine.andrianarison@gmail.com>
2026-07-10 17:25:17 +0200

    experimental: let claude run the research

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -4,6 +4,7 @@ from strategies.base import BaseStrategy
 from strategies.ema_crossover import EMACrossover
 from strategies.mean_reversion import MeanReversion
 from strategies.momentum import Momentum
+from strategies.volatility_target import VolatilityTargetOverlay, BuyAndHold
 from strategies.registry import StrategyRegistry
 
 __all__ = [
@@ -11,5 +12,7 @@ __all__ = [
     "EMACrossover",
     "MeanReversion",
     "Momentum",
+    "VolatilityTargetOverlay",
+    "BuyAndHold",
     "StrategyRegistry",
 ]
